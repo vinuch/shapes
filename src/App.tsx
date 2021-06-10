9280cf3b0cf0724745909c76ec136855ad4b2824
vinuch <vincentedeh42@gmail.com>
2021-06-10 18:46:51 +0100

    feat: create Navbar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,38 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  // Link
+} from "react-router-dom";
+
 import './App.css';
-import Button from './Components/Button'
+// import Button from './Components/Button'
+import Navbar from './Components/Navbar'
+import Home from './Pages/Home';
+import Login from './Pages/Login';
+import Signup from './Pages/Signup';
 
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      Hello world
-      <div>
-        <Button>Normal</Button>
-        <Button primary>Primary</Button>
-      </div>
+      <Navbar />
+
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/signup">
+          <Signup />
+        </Route>
+      </Switch>
     </div>
+  </Router>
   );
 }
 
